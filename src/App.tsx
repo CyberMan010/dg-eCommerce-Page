03056778b16116b139b7d570670a7ee0add13ff6
CyberMan010 <mohamahasoun60@gmail.com>
2025-02-18 13:33:22 +0300

    feat: navbar with subNavbar done added and styled

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,6 @@ function App() {
     <Router>
     <div className="App">
       <DgNavbar />
-      <Routes>
-        <Route path="/" element={<Testing />} />
-        <Route path="/about" element={<Testing />} />
-        <Route path="/portfolio" element={<Testing />} />
-        <Route path="/contact" element={<Testing />} />
-      </Routes>
     </div>
   </Router>
   )
