7b871f2c7fba8ee5d73d5b5465fa82d8b0baeea0
CyberMan010 <mohamahasoun60@gmail.com>
2025-02-21 01:30:01 +0300

    fix: navbar distances and look fixed

--- src/dg-Navbar/dg-Navbar.tsx
+++ src/dg-Navbar/dg-Navbar.tsx
@@ -1,5 +1,5 @@
-import "./dg-Navbar_style.scss"
 import { useState } from "react"
+import "./dg-Navbar_style.scss"
 
 export const DgNavbar = () => {
   const [menuOpen, setMenuOpen] = useState(false)
@@ -8,115 +8,58 @@ export const DgNavbar = () => {
     setMenuOpen(!menuOpen)
   }
 
-  return (
-    <header className="navbar">
-      <div className="navbar__top">
+
+    return (
+      <nav className="navbar">
         <div className="navbar__container">
-          <div className="navbar__start">
-            {/* Mobile Menu Button */}
-            <button className="navbar__mobile-toggle" onClick={handleMenuToggle}>
-              <img src="/category-2.svg" alt="Menu" />
-            </button>
-
-            <div className="navbar__logo">
-              <img src="/Logo.svg" alt="Safeer Logo" />
-            </div>
-
-            <div className="navbar__location">
-              <img src="/location.svg" alt="Location icon" />
-              <div className="navbar__location-text">
-                <span>Deliver To</span>
-                <strong>Jordan</strong>
-              </div>
+          <img src="/Logo.svg" alt="Safeer" className="navbar__logo" />
+  
+          <div className="navbar__location">
+            <img src="/location.svg" alt="Location icon" />
+            <div className="navbar__location-text">
+              <span>Deliver To</span>
+              <strong>Jordan</strong>
             </div>
           </div>
-
+  
           <div className="navbar__search">
             <div className="navbar__search-select">
               <span>All</span>
-              <img src="/arrow-down.svg" alt="Arrow down" />
+              <img src="/arrow-down.svg" alt="Select category" />
             </div>
-            <input type="text" placeholder="What are you looking for ?" className="navbar__search-input" />
+            <input type="text" placeholder="What are you looking for?" className="navbar__search-input" />
             <button className="navbar__search-button">
               <img src="/search-normal.svg" alt="Search" />
             </button>
           </div>
-
-          <div className="navbar__end">
-            <button className="navbar__language">
-              <img src="/language-square.svg" alt="Language" />
-              <span>العربية</span>
-            </button>
-
-            <div className="navbar__auth">
-              <img src="/profile-circle.svg" alt="Profile" />
-              <span>Login / Sign up</span>
-            </div>
-
-            <div className="navbar__actions">
-              <button className="navbar__action-button">
-                <div className="navbar__notification">
-                  <img src="/Ellipse 1.svg" alt="" />
-                  <span>2</span>
-                </div>
-                <img src="/arrange-circle-2.svg" alt="Trade" />
-              </button>
-
-              <button className="navbar__action-button">
-                <div className="navbar__notification">
-                  <img src="/Ellipse 1.svg" alt="" />
-                  <span>2</span>
-                </div>
-                <img src="/heart.svg" alt="Wishlist" />
-              </button>
-
-              <button className="navbar__action-button">
-                <div className="navbar__notification">
-                  <img src="/Ellipse 1.svg" alt="" />
-                  <span>2</span>
-                </div>
-                <img src="/bag.svg" alt="Cart" />
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <nav className="navbar__bottom">
-        <div className="navbar__container">
-          <button className="navbar__categories">
-            <img src="/category-2.svg" />
-            <p>All Categories</p>
-            <img src="/arrow-down (1).svg" alt="Arrow down" />
+  
+          <button className="navbar__language">
+            <img src="/language-square.svg" alt="Language" />
+            <span>العربية</span>
           </button>
-
-          <div className="navbar__links">
-            <a href="#">Today's Deals</a>
-            <a href="#">Brand Store</a>
-            <a href="#">Electronics</a>
-            <a href="#">Home & Kitchen</a>
-            <a href="#">Fashion</a>
-            <a href="#">Baby & Toys</a>
-            <a href="#">Beauty Care</a>
+  
+          <button className="navbar__auth">
+            <img src="/profile-circle.svg" alt="Profile" />
+            <span>Login / Sign up</span>
+          </button>
+  
+          <div className="navbar__actions">
+            <button className="navbar__action-button">
+              <div className="navbar__notification">2</div>
+              <img src="/arrange-circle-2.svg" alt="Trade" />
+            </button>
+  
+            <button className="navbar__action-button">
+              <div className="navbar__notification">2</div>
+              <img src="/heart.svg" alt="Wishlist" />
+            </button>
+  
+            <button className="navbar__action-button">
+              <div className="navbar__notification">2</div>
+              <img src="/bag.svg" alt="Cart" />
+            </button>
           </div>
         </div>
       </nav>
-
-      {/* Mobile menu */}
-      <div className={`navbar__mobile-menu ${menuOpen ? "active" : ""}`}>
-        <button className="navbar__mobile-close" onClick={handleMenuToggle}>
-          ✖ Close
-        </button>
-        <div className="navbar__mobile-links">
-          <a href="#">Today's Deals</a>
-          <a href="#">Brand Store</a>
-          <a href="#">Electronics</a>
-          <a href="#">Home & Kitchen</a>
-          <a href="#">Fashion</a>
-          <a href="#">Baby & Toys</a>
-          <a href="#">Beauty Care</a>
-        </div>
-      </div>
-    </header>
-  )
-}
+    )
+  }
